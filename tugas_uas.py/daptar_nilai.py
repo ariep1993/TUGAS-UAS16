ed7fcce3e39381531e1c5e67a27345d4bd82d066
ariep1993 <arieptea@gmail.com>
2023-01-06 20:44:14 +0700

    projeku_uas

---
+++ tugas_uas.py/daptar_nilai.py
@@ -0,0 +1,25 @@
+menutabel()
+
+def __init__(self, nim, nama, tugas, uts, uas):
+        self.nim = nim
+        self.nama = nama
+        self.tugas = tugas
+        self.uts = uts
+        self.uas = uas
+
+def tambah(self, nim, nama, tugas, uts, uas):
+        data.nim.append(nim)
+        data.nama.append(nama)
+        data.tugas.append(tugas)
+        data.uts.append(uts)
+        data.uas.append(uas)
+
+def lihat(self):
+        for i in range(len(data.nama)):
+            print("|", i + 1, "  |", end="")
+            print('{0:<25}'.format(self.nama[i]), end="")
+            print("|", self.nim[i], end="")
+            print(" |", self.tugas[i], end="")
+            print("    |", self.uts[i], end="")
+            print("  |", self.uas[i], " | ", end="")
+            print(f'{((self.tugas[i] * 30 / 100) + (self.uts[i] * 35 / 100) + (self.uas[i] * 35 / 100)) :.2f}', " |")
